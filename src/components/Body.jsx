32ba254ae42626637d7a5ede644f1e8e66ba759f
martin <tinbonino@gmail.com>
2024-10-14 16:48:03 -0300

    prueba 2

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -4,13 +4,13 @@ function Body() {
       <main className="flex-grow p-4">
         <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
           <div className="aspect-w-16 aspect-h-9">
-            <img src="/assets/image1a.jpeg" alt="Image 1" className="object-cover w-full h-full" />
+            <img src="/src/assets/image1a.jpeg" alt="Image 1" className="object-cover w-full h-full" />
           </div>
           <div className="aspect-w-16 aspect-h-9">
-            <img src="/assets/image1a.jpeg" alt="Image 2" className="object-cover w-full h-full" />
+            <img src="/src/assets/image2a.jpeg" alt="Image 2" className="object-cover w-full h-full" />
           </div>
           <div className="aspect-w-16 aspect-h-9">
-            <img src="/assets/image1a.jpeg" alt="Image 3" className="object-cover w-full h-full" />
+            <img src="/src/assets/image3a.jpeg" alt="Image 3" className="object-cover w-full h-full" />
           </div>
         </div>
       </main>
